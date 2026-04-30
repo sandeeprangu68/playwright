d0270173c8649bc6b26f52e65750072d0de6602b
sandeeprangu68 <58335443+sandeeprangu68@users.noreply.github.com>
2026-04-30 13:34:59 +0530

    Update homepage.spec.js

--- tests/homepage.spec.js
+++ tests/homepage.spec.js
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('Verify page title', async ({ page }) => {
-  await page.goto('https://sandeepprangu68.github.io/playwright/');
+  await page.goto('https://sandeeprangu68.github.io/playwright/');
 
   await expect(page).toHaveTitle('Test App');
 });
